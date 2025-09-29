79beb88e0b338bbe23010f14eab035d45eff0874
secretsantamatch <santa@secretsantamatch.com>
2025-09-29 19:36:48 -0400

    Update ShareButtons.tsx

--- src/components/ShareButtons.tsx
+++ src/components/ShareButtons.tsx
@@ -7,11 +7,19 @@ const PinterestIcon = () => <svg xmlns="http://www.w3.org/2000/svg" width="24" h
 const LinkIcon = () => <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z" clipRule="evenodd" /></svg>;
 const CheckIcon = () => <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" /></svg>;
 
+interface ShareButtonsProps {
+    participantCount?: number;
+}
 
-const ShareButtons: React.FC = () => {
+const ShareButtons: React.FC<ShareButtonsProps> = ({ participantCount }) => {
   const [copySuccess, setCopySuccess] = useState('');
   const url = 'https://secretsantamatch.com/';
-  const text = 'I just used this free Secret Santa Generator! It was super easy and perfect for our gift exchange. Check it out:';
+  
+  let text = 'I just used this free Secret Santa Generator! It was super easy and perfect for our gift exchange. Check it out:';
+  if (participantCount && participantCount > 1) {
+    text = `I just organized our Secret Santa for ${participantCount} people with this free generator! It was so easy. Check it out:`;
+  }
+  
   const imageUrl = 'https://secretsantamatch.com/wp-content/uploads/2023/09/Secret-Santa-Match.com-icon.png';
 
   const encodedUrl = encodeURIComponent(url);
