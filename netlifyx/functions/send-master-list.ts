02a43b2faaf4404c420191ce7acdd3516e6d36f7
secretsantamatch <santa@secretsantamatch.com>
2025-09-29 22:05:35 -0400

    Delete netlifyx/functions directory

--- netlifyx/functions/send-master-list.ts
+++
@@ -1,8 +0,0 @@
-// This function is disabled as the application has moved to a URL-based sharing system.
-// It is kept to prevent build errors from missing files in some deployment environments.
-export async function handler(event: any, context: any) {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "This function is disabled." }),
-    };
-}
