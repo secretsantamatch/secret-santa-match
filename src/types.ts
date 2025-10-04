cd22907f9f376ce4546e4f12e33c6e498218af45
secretsantamatch <santa@secretsantamatch.com>
2025-10-04 19:05:36 -0400

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -59,6 +59,5 @@ export interface ExchangeData {
     style: CardStyleData;
     e?: string; // eventDetails
     rd?: string; // revealDate
-    rt?: string; // revealTime
     th?: string; // theme
 }
