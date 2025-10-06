43d59a1ef1c805c32171bc9b80ad48c4cfc464eb
secretsantamatch <santa@secretsantamatch.com>
2025-10-05 21:10:28 -0400

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -39,7 +39,6 @@ const Footer: React.FC = () => {
                         &copy; {currentYear} SecretSantaMatch.com
                     </div>
                     <div className="order-1 sm:order-2">
-                        <a href="https://blog.secretsantamatch.com" target="_blank" rel="noopener noreferrer" className="font-semibold hover:text-slate-900 transition-colors px-3 py-2">Blog</a>
                         <a href="/privacy-policy.html" className="font-semibold hover:text-slate-900 transition-colors px-3 py-2">Privacy Policy</a>
                     </div>
                 </div>
