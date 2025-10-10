83911fd0bdd18dc66172c50e67088dfe327a0294
secretsantamatch <santa@secretsantamatch.com>
2025-10-09 21:30:55 -0400

    Update resources.ts

--- src/data/resources.ts
+++ src/data/resources.ts
@@ -1,9 +1,10 @@
 
 export interface Resource {
   id: string;
-  type: 'Free Download' | 'Guide & Tips' | 'Article';
+  type: 'Free Download' | 'Guide & Tips' | 'Article' | 'Guide & Printable';
   title: string;
   description: string;
   thumbnailUrl: string;
   linkUrl: string;
+  lastUpdated?: string;
 }
