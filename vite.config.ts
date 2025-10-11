09e611c70930aaf92b9ee0e28a2a3ccc67b30ae7
secretsantamatch <santa@secretsantamatch.com>
2025-10-11 18:54:02 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,8 +8,8 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: 'index.html',
-        blog: 'public/blog.html'
+        main: resolve(__dirname, 'index.html'),
+        blog: resolve(__dirname, 'blog.html')
       }
     }
   },
