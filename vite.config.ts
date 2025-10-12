b8fffa88d3ec167f85117dfb64aa5531e1c6af39
secretsantamatch <santa@secretsantamatch.com>
2025-10-12 13:11:15 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+// FIX: __dirname is not available in ES modules. This creates an equivalent.
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
