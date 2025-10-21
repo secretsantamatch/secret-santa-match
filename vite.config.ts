736633b119ee4d58e8d8a8ae8b94db188924c11a
secretsantamatch <santa@secretsantamatch.com>
2025-10-21 17:05:28 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
+import { fileURLToPath, URL } from 'url'
+
+// FIX: __dirname is not available in ES modules. This defines it for use in this file.
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +14,8 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         blog: resolve(__dirname, 'blog.html'),
-        'minimum-payment-calculator': resolve(__dirname, 'minimum-payment-calculator.html')
+        'minimum-payment-calculator': resolve(__dirname, 'minimum-payment-calculator.html'),
+        'holiday-budget-calculator': resolve(__dirname, 'holiday-budget-calculator.html')
       }
     }
   },
