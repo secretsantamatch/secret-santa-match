d07e1a08dd8155c6bb9eab79c1775a927e545c52
secretsantamatch <santa@secretsantamatch.com>
2025-10-24 22:02:43 -0400

    Update blog.tsx

--- src/blog.tsx
+++ src/blog.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import BlogPage from './components/BlogPage';
-import './index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
