5585c4fce6da7ef39ce170653dc1eae0f1207e5b
secretsantamatch <santa@secretsantamatch.com>
2025-10-31 07:39:23 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         generator: resolve(__dirname, 'generator.html'),
+        'about-us': resolve(__dirname, 'public/about-us.html'),
         'minimum-payment-calculator': resolve(__dirname, 'minimum-payment-calculator.html'),
         'holiday-budget-calculator': resolve(__dirname, 'holiday-budget-calculator.html')
       }
