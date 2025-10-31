30ca6c6e02326591b2009252ba7b07e81cf076c3
secretsantamatch <santa@secretsantamatch.com>
2025-10-31 07:37:58 -0400

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,18 @@ if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      })
+      .catch(error => {
+        console.log('ServiceWorker registration failed: ', error);
+      });
+  });
+}
+
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
