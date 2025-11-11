d46ec85beffb1af73b092aabfd23e13e4a278123
secretsantamatch <santa@secretsantamatch.com>
2025-11-10 21:44:01 -0500

    zz98

--- src/components/PrintableCard.tsx
+++ src/components/PrintableCard.tsx
@@ -99,7 +99,7 @@ const PrintableCard: React.FC<PrintableCardProps> = ({
                 <div className="absolute inset-0 bg-white"></div>
             )}
             <div
-                className={`absolute inset-0 pt-10 pb-8 px-8 flex flex-col text-center justify-center gap-y-4 ${currentFontSize.base} ${fontClasses[font]}`}
+                className={`absolute inset-0 pt-16 pb-8 px-8 flex flex-col text-center justify-center gap-y-4 ${currentFontSize.base} ${fontClasses[font]}`}
                 style={{
                     color: txtColor,
                     textShadow: textShadow,
@@ -126,12 +126,12 @@ const PrintableCard: React.FC<PrintableCardProps> = ({
                     {isNameRevealed && hasDetails && (
                         <>
                             <h3 className={`font-bold text-lg mb-2`}>{wish}</h3>
-                             <ul className="space-y-1 list-none p-0 m-0 inline-block text-left">
+                             <ul className="space-y-1 list-none p-0 m-0 inline-block text-center">
                                 {receiver.budget && <li><strong className="font-semibold">Budget:</strong> {receiver.budget}</li>}
                                 {receiver.interests && <li><strong className="font-semibold">Interests:</strong> {receiver.interests}</li>}
                                 {receiver.likes && <li><strong className="font-semibold">Likes:</strong> {receiver.likes}</li>}
                                 {receiver.dislikes && <li><strong className="font-semibold">Dislikes:</strong> {receiver.dislikes}</li>}
-                                {receiver.links && <li className="list-none"><strong className="font-semibold">Links:</strong><ul className="pl-4">{renderLinks(receiver.links)}</ul></li>}
+                                {receiver.links && <li className="list-none"><strong className="font-semibold">Links:</strong><ul className="pl-4 text-left">{renderLinks(receiver.links)}</ul></li>}
                             </ul>
                         </>
                     )}
