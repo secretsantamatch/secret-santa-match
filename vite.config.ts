6fb996a165d2369465f4b5fc62c082415b9b8c3e
secretsantamatch <santa@secretsantamatch.com>
2025-12-09 07:25:25 -0500

    sdfd

--- vite.config.ts
+++ vite.config.ts
@@ -30,6 +30,7 @@ export default defineConfig({
         'ultimate-guide-buying-gift-cards': resolve(__dirname, 'ultimate-guide-buying-gift-cards.html'),
         'best-skincare-gifts': resolve(__dirname, 'best-skincare-gifts.html'),
         'potluck-planner': resolve(__dirname, 'potluck-planner.html'),
+        'potluck-organizer-ideas-recipes-2026': resolve(__dirname, 'potluck-organizer-ideas-recipes-2026.html'),
       }
     }
   },
